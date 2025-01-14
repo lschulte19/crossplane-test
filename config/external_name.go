21b846c6548f579d678cd350313ce5eb5ec40936
lschulte19 <ladandecsm@gmail.com>
2025-01-14 15:40:13 -0300

    first commit

--- config/external_name.go
+++ config/external_name.go
@@ -1,16 +1,33 @@
-/*
-Copyright 2022 Upbound Inc.
-*/
+*
+ * Copyright (c) 2023 Oracle and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package config
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"github.com/lschulte19/crossplane-test/config/common"
+	"github.com/crossplane/upjet/pkg/config"
+)
 
 // ExternalNameConfigs contains all external name configurations for this
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
-	"null_resource": config.IdentifierFromProvider,
+	"oci_objectstorage_bucket":                        config.IdentifierFromProvider,
+	"oci_objectstorage_object":                        config.IdentifierFromProvider,
+	"oci_objectstorage_object_lifecycle_policy":       config.IdentifierFromProvider,
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
@@ -20,6 +37,7 @@ func ExternalNameConfigurations() config.ResourceOption {
 	return func(r *config.Resource) {
 		if e, ok := ExternalNameConfigs[r.Name]; ok {
 			r.ExternalName = e
+			r.Version = common.VersionAlpha1
 		}
 	}
 }
--- config/provider.go
+++ config/provider.go
@@ -1,6 +1,3 @@
-/*
-Copyright 2021 Upbound Inc.
-*/
 
 package config
 
@@ -10,12 +7,32 @@ import (
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/lschulte19/crossplane-test/config/artifacts"
+	"github.com/lschulte19/crossplane-test/config/certificatesmanagement"
+	"github.com/lschulte19/crossplane-test/config/containerengine"
+	"github.com/lschulte19/crossplane-test/config/core"
+	"github.com/lschulte19/crossplane-test/config/dns"
+	"github.com/lschulte19/crossplane-test/config/events"
+	"github.com/lschulte19/crossplane-test/config/filestorage"
+	"github.com/lschulte19/crossplane-test/config/functions"
+	"github.com/lschulte19/crossplane-test/config/healthchecks"
+	"github.com/lschulte19/crossplane-test/config/identity"
+	"github.com/lschulte19/crossplane-test/config/kms"
+	"github.com/lschulte19/crossplane-test/config/loadbalancer"
+	"github.com/lschulte19/crossplane-test/config/logging"
+	"github.com/lschulte19/crossplane-test/config/monitoring"
+	"github.com/lschulte19/crossplane-test/config/networkfirewall"
+	"github.com/lschulte19/crossplane-test/config/networkloadbalancer"
+	"github.com/lschulte19/crossplane-test/config/objectstorage"
+	"github.com/lschulte19/crossplane-test/config/ons"
+	"github.com/lschulte19/crossplane-test/config/servicemesh"
+	"github.com/lschulte19/crossplane-test/config/streaming"
+	"github.com/lschulte19/crossplane-test/config/vault"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "oci"
+	modulePath     = "github.com/oracle/provider-oci"
 )
 
 //go:embed schema.json
@@ -27,16 +44,35 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("template.upbound.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
-		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
 			ExternalNameConfigurations(),
 		))
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		objectstorage.Configure,
+		identity.Configure,
+		core.Configure,
+		kms.Configure,
+		containerengine.Configure,
+		artifacts.Configure,
+		ons.Configure,
+		networkloadbalancer.Configure,
+		dns.Configure,
+		healthchecks.Configure,
+		functions.Configure,
+		networkfirewall.Configure,
+		logging.Configure,
+		monitoring.Configure,
+		loadbalancer.Configure,
+		servicemesh.Configure,
+		certificatesmanagement.Configure,
+		filestorage.Configure,
+		events.Configure,
+		vault.Configure,
+		events.Configure,
+		streaming.Configure,
 	} {
 		configure(pc)
 	}
